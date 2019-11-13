b200e0ff38bbf11623783ecbfd0f89bb3bbbf7d1
Cano Espinoza <kabacano@yahoo.com>
2019-11-12 23:00:59 -0800

    adds links/icons, resume pdf, and more styling

--- src/js/components/Body.jsx
+++ src/js/components/Body.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
-import profilePic from './images/profilePic.JPG'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLinkedin, faGithub, faSoundcloud, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import profilePic from './images/profilePic.JPG';
+import resume from './documents/Resume(CanoEspinoza)V2.pdf';
 
 export default class Body extends React.Component {
 
@@ -12,24 +14,29 @@ export default class Body extends React.Component {
             <div id='body'>
               <img id='profilePic' src={profilePic}/>
               <h2 className='subHeader'>Technologies</h2>
-                <ul>
-                  <li className='list'>Javascript</li>
-                  <li className='list'>NodeJS</li>
-                  <li className='list'>Agile/SCRUM</li>
-                  <li className='list'>React/Redux</li>
-                  <li className='list'>CSS</li>
-                  <li className='list'>Bootstrap</li>
-                  <li className='list'>HTML</li>
-                  <li className='list'>CSS</li>
-                  <li className='list'>Express</li>
-                  <li className='list'>Git</li>
-                  <li className='list'>JIRA</li>
-                  <li className='list'>Mocha</li>
-                  <li className='list'>MongoDB</li>
-                  <li className='list'>Loopback</li>
-                  <li className='list'>MySQL</li>
-                </ul>
-              <h2 className='subHeader col-4'>Links</h2>  
+                <p className='listText'>
+                  Javascript,  NodeJS,  Agile/SCRUM,  React/Redux,  CSS,  
+                  Bootstrap,  HTML,  Express,  Git,  JIRA,  
+                  Mocha,  MongoDB,  Loopback, MySQL
+                </p>
+              <h2 className='subHeader'>Links</h2> 
+                <p id='links'>
+                  <a className='icon' href='https://www.linkedin.com/in/cano-espinoza-b8a343193/' target='_blank'>
+                    <FontAwesomeIcon icon={faLinkedin}/>
+                  </a>
+                  <a className='icon' href='https://github.com/canoespinoza' target='_blank'>
+                    <FontAwesomeIcon icon={faGithub}/>
+                  </a>
+                  <a className='icon' href='https://soundcloud.com/che_beats' target='_blank'>
+                    <FontAwesomeIcon icon={faSoundcloud}/>
+                  </a>
+                  <a className='icon' href='https://www.youtube.com/channel/UCaaIItB3QrC1zfU8FiHeeCA' target='_blank'>
+                    <FontAwesomeIcon icon={faYoutube}/>
+                  </a>
+                </p>
+              <a id='resumeLink' href={resume} target='_blank'>
+                <h2 id='resume'>Resume</h2>
+              </a>  
             </div>
           </div>
         </div>
