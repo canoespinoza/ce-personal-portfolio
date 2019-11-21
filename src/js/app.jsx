a2a3d3b281704b46db26c78e668bcc822dbc1571
Cano Espinoza <kabacano@yahoo.com>
2019-11-20 16:37:21 -0800

    troubleshooting deployment build issue

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from './components/Header';
+//import Header from './components/Header';
 import Sticky from 'react-sticky-el';
 import Body from './components/Body';
 import Projects from './components/Projects'
