084a7fdc3dd97fd758d7f77355394f1426a83d19
Cano Espinoza <kabacano@yahoo.com>
2019-11-20 17:18:09 -0800

    moves header position in app.jsx

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from './components/Header';
 import Sticky from 'react-sticky-el';
+import Header from './components/Header';
 import Body from './components/Body';
 import Projects from './components/Projects'
 
