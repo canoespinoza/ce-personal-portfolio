b9c48a620dabf0b971feb9466ec52feaea55e8db
Cano Espinoza <kabacano@yahoo.com>
2019-11-22 16:49:11 -0800

    changes header name

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import Body from './components/Body.jsx';
-import Projects from './components/Projects.jsx';
-import Header from './components/Header.jsx';
+import CEheader from './components/CEheader'
+import Body from './components/Body';
+import Projects from './components/Projects';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <CEheader/>
         <Body/>
         <Projects/>
       </div>
--- src/js/components/CEheader.jsx
+++ src/js/components/CEheader.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-// import {faLinkedin, faGithub, faSoundcloud, faYoutube} from '@fortawesome/free-brands-svg-icons';
-// import resume from './documents/Resume(CanoEspinoza)V2.pdf';
-// import logo from './images/CElogo1.png';
 
-export default class Header extends React.Component {
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLinkedin, faGithub, faSoundcloud, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import resume from './documents/Resume(CanoEspinoza)V2.pdf';
+import logo from './images/CElogo1.png';
+
+export default class CEheader extends React.Component {
 
   render() {
     return (
       <div className='overallContainer'>
         <div className='headerContainer'>
             <div id='header'>
-              {/* <img id='logo' src={logo}/> */}
+              <img id='logo' src={logo}/>
               <div id='title'>Cano Espinoza</div>
-              {/* <div id='links'>
+              <div id='links'>
                 <a id='resumeLink' href={resume} target='_blank'>
                   <div id='resume'>Resume</div>
                 </a>  
@@ -29,7 +30,7 @@ export default class Header extends React.Component {
                 <a className='icon' href='https://www.youtube.com/channel/UCaaIItB3QrC1zfU8FiHeeCA' target='_blank'>
                   <FontAwesomeIcon icon={faYoutube}/>
                 </a>
-              </div>    */}
+              </div>   
             </div>
         </div>
       </div>
