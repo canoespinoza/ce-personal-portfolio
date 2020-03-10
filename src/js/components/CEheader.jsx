4abba7608e0af4a5504b5cf3ee604770e2cbae7d
Cano Espinoza <kabacano@yahoo.com>
2020-03-09 19:29:01 -0700

    updates resume, adds favicon

--- src/js/components/CEheader.jsx
+++ src/js/components/CEheader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faLinkedin, faGithub, faSoundcloud, faYoutube} from '@fortawesome/free-brands-svg-icons';
-import resume from './documents/Resume(CanoEspinoza)V2.pdf';
+import resume from './documents/Resume(CanoEspinoza).pdf';
 import logo from './images/CElogo1.png';
 
 export default class CEheader extends React.Component {
